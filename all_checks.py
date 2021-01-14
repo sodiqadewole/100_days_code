921d3081fa2589fec16f16718f4af0fa0d750118
sodiqadewole <soa2wg@virginia.edu>
2021-01-13 20:13:09 -0500

    added an all checks file

---
+++ all_checks.py
@@ -0,0 +1,12 @@
+import os
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    pass
+
+main()
+
+import numpy as np
