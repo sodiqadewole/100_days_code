0787c6fe692291773925077e63c2447a2d77f8ee
sodiqadewole <soa2wg@virginia.edu>
2021-01-16 07:54:08 -0500

    Modified and added a script

---
+++ 100_days_code/leap_year.py
@@ -0,0 +1,19 @@
+def leap_year():
+    """Determine if a given year is a leap year"""
+    year = int(input("Enter a year: "))
+    if year % 4 == 0:
+        if year % 100 == 0:
+            message = "This is not a leap year!"
+            if year % 400 == 0:
+                message = "This is a leap year"
+            else:
+                message = "This is not a leap year!"
+        else:
+            message = "This is a leap year!"
+    else:
+        message = "This is not a leap year!"
+
+    print(message)
+
+if __name__ == '__main__':
+    leap_year()
