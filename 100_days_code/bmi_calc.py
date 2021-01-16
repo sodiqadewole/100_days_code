e6003ba9b538870bf975d9a3da9779f27a220070
sodiqadewole <soa2wg@virginia.edu>
2021-01-15 23:08:21 -0500

    fix bug

--- 100_days_code/bmi_calc.py
+++ 100_days_code/bmi_calc.py
@@ -1,11 +1,11 @@
 def calculate_bmi():
-    """This module computes users bmi"
+    """This module computes users bmi"""
     height = float(input("Enter your height (m): "))
     weight = float(input("Enter your weight (kg): "))
 
-    bmi = weight/height
+    bmi = weight/height**2
 
-    print("Your BMI is: {}".float(bmi))
+    print("Your BMI is: {:.1f}".format(bmi))
 
 if __name__ == '__main__':
     calculate_bmi()
