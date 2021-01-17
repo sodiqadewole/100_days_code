cd808daa52f561c4e7dc1c4b48fc2d050ed1a256
sodiqadewole <soa2wg@virginia.edu>
2021-01-16 23:01:13 -0500

    100_days_code

--- bmi_calc.py
+++ bmi_calc.py
@@ -4,8 +4,18 @@ def calculate_bmi():
     weight = float(input("Enter your weight (kg): "))
 
     bmi = weight/height**2
-
-    print("Your BMI is: {:.1f}".format(bmi))
+    if bmi < 18.5:
+        message = "You are underweight"
+    elif bmi <= 25:
+        messaeg = "You have normal weight"
+    elif bmi <= 30:
+        message = "You are overweight"
+    elif bmi <= 35:
+        message = "You are Obese"
+    else:
+        message = "You are clinically obese"
+    print("Your BMI is: {:.2f}".format(bmi))
+    print(message)
 
 if __name__ == '__main__':
     calculate_bmi()
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,9 +1,9 @@
 def tip_calculator():
     greetings = "Welcome to the tip calculator "
     print(greetings)
-    bill = float(input("What was the total bill?  "))
+    bill = float(input("What was the total bill? $"))
     percent_tip = int(input("What percentage of tip would you like to give?  "))
-    persons = int(input("How many people to split the bill?  "))
+    persons = int(input("How many people to split the bill? $"))
 
     tip = (bill*percent_tip)/100
     total_bill = bill + tip
