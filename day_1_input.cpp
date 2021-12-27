f74fe6f7a59824f3f159069a6c177f12738336a0
sodiqadewole <adewolesodiq@gmail.com>
2021-12-27 08:38:59 -0800

    added input function using cin>>

---
+++ day_1_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name = "";
+    cout << "What is your name: " << endl;
+    cin >> name;
+    cout << "Your name is: " << name << endl;
+    return 0;
+}
