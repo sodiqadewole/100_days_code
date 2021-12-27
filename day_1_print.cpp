0a33bcc85c3ec719de0a2fed895eae68b063959d
sodiqadewole <adewolesodiq@gmail.com>
2021-12-27 08:16:25 -0800

    added day-1 cpp

--- day_1_print.cpp
+++ day_1_print.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl << endl;
+    cout << "This is Day - 1 C++ print function." << endl;
+    cout << "The print function is declared as follows: " << endl;
+    cout << "cout << what to print << endl;" << endl << endl;
+    cout << "Thanks for reviewing this code." << endl;
+
     return 0;
 }
