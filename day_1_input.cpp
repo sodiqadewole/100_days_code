351a4de094715a6a8a34f22d680d824ad9113084
sodiqadewole <adewolesodiq@gmail.com>
2021-12-27 08:43:02 -0800

    added input function using cin>>

--- day_1_input.cpp
+++ day_1_input.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main(){
-    string name = "";
-    cout << "What is your name: " << endl;
-    cin >> name;
-    cout << "Your name is: " << name << endl;
+    // Declare the string variables
+    string FName, LName;
+    // Get the first name
+    cout << "What is your First Name: " << endl;
+    cin >> FName;
+    // Get the last name
+    cout << "What is your Last Name: " << endl;
+    cin >> LName;
+    // Print first name and last name to console
+    cout << "Your name is: " << FName << " " << LName << endl;
+    cout << "Thank you for using this program!" << endl;
     return 0;
 }
