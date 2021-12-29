5d7d0853ebe27192e1d511ed9ccce1ded853606e
sodiqadewole <adewolesodiq@gmail.com>
2021-12-29 08:28:38 -0800

    new commit

--- day2_life_in_weeks.cpp
+++ day2_life_in_weeks.cpp
@@ -13,6 +13,9 @@ int main(){
     weeks = 52 * years_left;
     days = 365 * years_left;
 
-    cout << "If you are going to live up to 90 years, you already lived for " << 52 * age; 
-    cout << " weeks and you have " << weeks << " weeks left." << endl;
+    cout << "If you are going to live up to 90 years, you already lived for " << 52 * age << " weeks" << endl; 
+    cout << "You have " << years_left << " years left." << endl;
+    cout << "You have " << months << " months left." << endl;
+    cout << "You have " << weeks << " weeks left." << endl;
+    cout << "You have " << days << " days left." << endl;
 }
