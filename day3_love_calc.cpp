09b91b1edd15e6d601aaa29a68dc54975d7289fe
sodiqadewole <adewolesodiq@gmail.com>
2021-12-31 23:40:23 -0800

    new commit

--- day3_love_calc.cpp
+++ day3_love_calc.cpp
@@ -8,42 +8,85 @@ using namespace std;
 int get_count(string to_search, char to_find)
 {
     int count = 0;
-    for (int i=0; to_search.length(); i++){
-        if (to_search.find(to_find)){
+    // cout << "Length of string to search is " << to_search.length() << endl;
+    for (int i=0; i<to_search.length(); i++){
+        if (to_search[i] == to_find){
             count += 1;
         }
     }
     return count;
 }
+
+
 int main()
 {
-    string user_name;
+    string user_name, fName, lName;
     string partner_name;
-    int idx;
-    int score = 0, total_score = 0;
-    string string_to_find = "TRUELOVE";
+    int t, r, u, e, l, o, v;
+    int true_score, love_score;
+    string total_score;
 
     cout << "Enter your name: ";
-    cin >> user_name;
+    cin >> fName;
+    cin >> lName;
+    user_name = fName + lName;
+    cout << "Your name is " << user_name << endl;
 
     // cout << "Enter your partner's name: ";
     // cin >> partner_name;
 
     // Loop through the entered user name
+    t = get_count(user_name, 't');
+    // cout << "t has a score " << t << endl;
+    r = get_count(user_name, 'r');
+    // cout << "r has a score " << r << endl;
+    u = get_count(user_name, 'u');
+    // cout << "u has a score " << u << endl;
+    e = get_count(user_name, 'e');
+    // cout << "e has a score " << e << endl;
 
-    for (int i=0; string_to_find.length(); i++)
-    {
-        score = get_count(user_name, string_to_find[i]);
-        total_score += score;
-        if (score > 0)
-        {
-            cout << string_to_find[i] << " found in " << user_name << endl;
-        } 
-        else 
-        {
-        cout << string_to_find[i] << " not found in " << user_name << endl;
-        }
-    }   
+    true_score = t + r + u + e;
 
-return total_score;
+    l = get_count(user_name, 'l');
+    o = get_count(user_name, 'o');
+    v = get_count(user_name, 'v');
+    e = get_count(user_name, 'e');
+
+    love_score = l + o + v + e;
+
+    total_score = to_string(true_score) + to_string(love_score);
+    cout << "Your score is " << total_score << endl;  
+
+return 0;
 }
+
+// void TraverseString(string &str, int N)
+//  {
+//     // Stores address of 
+//     // a character of str
+//     string:: iterator it;
+      
+//     // Traverse the string
+//     for (it = str.begin(); it != str.end();
+//                                    it++) {
+//         // Print current character
+//         cout<< *it<< " ";
+//     }
+// }
+
+// Function to traverse the string and
+// print the elements of the string
+// void TraverseString(string &str, int N)
+// {
+      
+//     // Stores address of 
+//     // a character of str
+//     string:: iterator it;
+      
+//     // Traverse the string
+//     for (it = str.begin(); it != str.end();
+//                                    it++) {
+//         // Print current character
+//         cout<< *it<< " ";
+//     }
+// }
